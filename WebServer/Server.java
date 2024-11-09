e0238e40b30acd972a5cd65775ae2cb8448388b0
Joseph Ferguson <99895281+JosephAFerguson@users.noreply.github.com>
2024-11-09 13:58:12 -0500

    Update Server.java

--- WebServer/Server.java
+++ WebServer/Server.java
@@ -2,15 +2,22 @@ package com.example;
 
 import javax.json.JsonObject;
 import javax.json.JsonReader;
+import javax.swing.*;//to be used for our later gui if added
 import javax.json.Json;
 import java.util.HashMap;
 import java.io.*;
 import java.net.*;
 
+final class CONSTANTS {
+    public final int MAX_USERS = 100;
+    public final int MAX_MESSAGES = 1000; 
+}
+
 public final class Server 
 {
     public static void main(String[] argv) throws Exception 
     {
+        MessageBoard messageBoard = new MessageBoard();
         int port = 6789;
 
         // Establish the listen socket
@@ -24,7 +31,7 @@ public final class Server
             Socket socket = serverSocket.accept();
 
             // Construct an object to process the HTTP request message
-            JSONRequest request = new JSONRequest(socket);
+            JSONRequest request = new JSONRequest(socket, messageBoard);
 
             // Create a new thread to process the request
             Thread thread = new Thread(request);
@@ -41,45 +48,113 @@ final class Message{
     String subject;
     String content;
 }
-final class MessageBoard{
-    private HashMap<Integer, Message> messages; // Store messages by ID
+final class MessageBoard 
+{
+    private CONSTANTS consts;
+    private HashMap<Integer, Message> messages;
     private Message[] messagesByDate;
-    private int ind;
-
+    private String[] users;
+    private int userInd;
+    private int messageInd;
 
     public MessageBoard() {
         messages = new HashMap<>();
-        messagesByDate = new Message[100];
-        ind = 0;
+        messagesByDate = new Message[consts.MAX_MESSAGES];
+        users = new String[consts.MAX_USERS];
+        userInd = 0;
+        messageInd = 0;
+    }
+
+    public boolean getUser(String username) {
+        for (int i = 0; i < userInd; i++) {
+            if (users[i].equals(username)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void addUser(String username) {
+        if (userInd < consts.MAX_USERS) {
+            users[userInd] = username;
+            userInd++;
+        } else {
+            System.out.println("Max user limit reached. Cannot add more users.");
+        }
+    }
+
+    public void removeUser(String username) {
+        for (int i = 0; i < userInd; i++) {
+            if (users[i].equals(username)) {
+                // Shift elements to the left
+                for (int j = i; j < userInd - 1; j++) {
+                    users[j] = users[j + 1];
+                }
+                users[userInd - 1] = null; 
+                userInd--;
+                break; 
+            }
+        }
     }
 
     public void addMessage(int id, Message message) {
-        messages.put(id, message);
-        messagesByDate[ind] = message;
-        ind++;
+        if (messageInd < consts.MAX_MESSAGES) {
+            messages.put(id, message);
+            messagesByDate[messageInd] = message;
+            messageInd++;
+        } else {
+            System.out.println("Max message limit reached. Cannot add more messages.");
+        }
+    }
+
+    public Message[] getLast2() {
+        Message[] retMessages = new Message[2];
+        if (messageInd >= 2) {
+            retMessages[0] = messagesByDate[messageInd - 1];
+            retMessages[1] = messagesByDate[messageInd - 2];
+        } else if (messageInd == 1) {
+            retMessages[0] = messagesByDate[messageInd - 1];
+            retMessages[1] = null; 
+        } else {
+            retMessages[0] = retMessages[1] = null; 
+        }
+        return retMessages;
     }
 
     public Message getMessage(int id) {
         return messages.get(id);
     }
 
-    //this might not be needed
     public void deleteMessage(int id) {
+        if (!messages.containsKey(id)) {
+            System.out.println("Message ID not found.");
+            return;
+        }
         messages.remove(id);
-        //here we need to traverse through Messages by date
+        for (int i = 0; i < messageInd; i++) {
+            if (messagesByDate[i].messageID == id) {
+                for (int j = i; j < messageInd - 1; j++) {
+                    messagesByDate[j] = messagesByDate[j + 1];
+                }
+                messagesByDate[messageInd - 1] = null; 
+                messageInd--;
+                break; 
+            }
+        }
     }
-
-    // Additional methods for managing users, etc.
 }
+    
 
 final class JSONRequest implements Runnable 
 {
     private Socket socket;
+    private MessageBoard messageBoard;
 
     // Constructor
-    public JSONRequest(Socket socket) 
+    public JSONRequest(Socket socket, MessageBoard messageBoard) 
     {
         this.socket = socket;
+        this.messageBoard = messageBoard;
     }
 
     // Implement the run() method of the Runnable interface
@@ -147,41 +222,16 @@ final class JSONRequest implements Runnable
         }
 
         // Send Response(s) JSON(s)
-        sendJsonResponse(out, jsonObject);  
+        buildJsonResponse(out, jsonObject);  
     }
-
-    private void sendJsonResponse(BufferedWriter out, JsonObject jsonObject) throws IOException 
+    private void sendErrorJsonResponse(BufferedWriter out, JsonObject jsonObject) throws IOException 
     {
+        //violates protocol
         JsonObject responseJson = Json.createObjectBuilder().build();
-        String type = jsonObject.getString("type");
-
-        if ("clientRequest".equals(type)) // Use .equals() to compare strings
-        {  
-            String action = jsonObject.getString("action");
-            //and so on
-            if("join".equals(action))
-            {
-                //try request-  add user to public message board 
-                responseJson = Json.createObjectBuilder(responseJson)
-                    .add("type", "ServerAffirm")
-                    .add("receivedData", jsonObject)
-                    .build();
-            }
-            else//handle others
-            {
-                responseJson = Json.createObjectBuilder(responseJson)
-                    .add("type", "ServerAffirm")
-                    .add("receivedData", jsonObject)
-                    .build();
-            }
-        } else {
-            System.out.print(type);
-            // If not a "join" type, deny the request
-            responseJson = Json.createObjectBuilder(responseJson)
-                .add("type", "ServerDeny")
-                .add("receivedData", jsonObject)
-                .build();
-        }
+        responseJson = Json.createObjectBuilder(responseJson)
+        .add("type", "ServerDeny")
+        .add("receivedData", jsonObject)
+        .build();
 
         // Convert the response JSON to string and send it
         String jsonResponse = responseJson.toString();
@@ -192,6 +242,70 @@ final class JSONRequest implements Runnable
         out.write(jsonResponse);
         out.flush();
         System.out.println("Sent response: " + jsonResponse);
+    }
+    private void sendJsonResponse(BufferedWriter out, JsonObject jsonObject) throws IOException 
+    {
+        // Convert the response JSON to string and send it
+        String jsonResponse = jsonObject.toString();
+        out.write("HTTP/1.1 200 OK\r\n");
+        out.write("Content-Type: application/json\r\n");
+        out.write("Content-Length: " + jsonResponse.length() + "\r\n");
+        out.write("\r\n");
+        out.write(jsonResponse);
+        out.flush();
+        System.out.println("Sent response: " + jsonResponse);
+    }
+    private void buildJsonResponse(BufferedWriter out, JsonObject jsonObject) throws IOException 
+    {
+        JsonObject responseJson = Json.createObjectBuilder().build();
+
+        if(!jsonObject.isEmpty()){
+            String type;
+            if(jsonObject.containsKey("type"))
+            {
+                type = jsonObject.getString("type");
+                if ("clientRequest".equals(type)) // Use .equals() to compare strings
+                {  
+                    if(jsonObject.containsKey("action"))
+                    {
+                        String action = jsonObject.getString("action");
+                        if("join".equals(action))
+                        {
+                            if(jsonObject.containsKey("username"))
+                            {
+                                String username = jsonObject.getString("username");
+                                //try request-  add user to public message board 
+                                if(!this.messageBoard.getUser(username))
+                                {
+                                    this.messageBoard.addUser(username);
+                                    responseJson = Json.createObjectBuilder(responseJson)
+                                    .add("type", "ServerAffirm")
+                                    .add("receivedData", jsonObject)
+                                    .build();
+                                    sendJsonResponse(out, responseJson);
+                                    return;
+                                }
+                                else
+                                {
+                                    //username already exists
+                                }
+                                
+                            }
+                            else
+                            {
+                                //does not contain a username - prompt for a username
+                            }
+                        }
+                        else//handle others
+                        {
+                            //handle other requests here
+                        }
+                    }
+                }
+            } 
+        }
+        sendErrorJsonResponse(out, jsonObject);
+        return;
     }
 
     private void sendNotFound(BufferedWriter out) throws IOException 
